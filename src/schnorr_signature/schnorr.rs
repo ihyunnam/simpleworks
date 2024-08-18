a5437c69402589856c5b7d56f8c3d3311d398ee3
Ihyun Nam <ihyunnam@DNa809943.SUNet>
2024-08-17 20:28:38 -0700

    .

--- src/schnorr_signature/schnorr.rs
+++ src/schnorr_signature/schnorr.rs
@@ -1,10 +1,10 @@
+use subtle::ConstantTimeEq as _;
 use ark_serialize::CanonicalSerialize;
 use serde::Serialize;
 use ark_ed_on_bls12_381::{EdwardsProjective, EdwardsParameters};
 
-type C = EdwardsProjective;
-type P = EdwardsParameters;
-type Fr = <C as ProjectiveCurve>::ScalarField;
+// type C = EdwardsProjective;
+// type P = EdwardsParameters;
 use std::collections::HashMap;
 
 use sha2::Digest as _;
@@ -18,11 +18,14 @@ use ark_ff::{
 use ark_std::io::{Result as IoResult, Write};
 use ark_std::rand::Rng;
 use ark_std::{hash::Hash, marker::PhantomData, vec::Vec};
+use ark_std::{One, Zero};
 use blake2::Blake2s;
 use digest::Digest;
-use crate::schnorr_signature::{AggregateSignatureScheme};
+// use crate::schnorr_signature::{AggregateSignatureScheme};
 use musig2::{
-    errors::KeyAggError, tagged_hashes::{KEYAGG_COEFF_TAG_HASHER, KEYAGG_LIST_TAG_HASHER, MUSIG_AUX_TAG_HASHER, MUSIG_NONCE_TAG_HASHER}, xor_bytes, NonceSeed
+    errors::{KeyAggError, RoundContributionError, SignerIndexError},
+    tagged_hashes::{KEYAGG_COEFF_TAG_HASHER, KEYAGG_LIST_TAG_HASHER, MUSIG_AUX_TAG_HASHER, MUSIG_NONCE_TAG_HASHER},
+    NonceSeed
 };
 
 use derivative::Derivative;
@@ -30,6 +33,8 @@ use derivative::Derivative;
 pub struct Schnorr<C: ProjectiveCurve> {
     _group: PhantomData<C>,
 }
+
+type Fr = <EdwardsProjective as ProjectiveCurve>::ScalarField;
 
 #[derive(Derivative)]
 #[derivative(Clone(bound = "C: ProjectiveCurve"), Debug)]
@@ -41,7 +46,7 @@ pub struct Parameters<C: ProjectiveCurve> {
 pub type PublicKey<C> = <C as ProjectiveCurve>::Affine;
 
 /* ADDED BY ME FOR MUSIG2. */
-pub type Point = PublicKey::<C>;
+pub type Point = PublicKey<EdwardsProjective>;
 
 #[derive(Clone, Default, Debug)]
 pub struct SecretKey<C: ProjectiveCurve> {
@@ -105,7 +110,7 @@ where
         // (k, e);
         let (random_scalar, verifier_challenge) = {
             // Sample a random scalar `k` from the prime scalar field.
-            let random_scalar: C::ScalarField = C::ScalarField::rand(rng);
+            let random_scalar  = C::ScalarField::rand(rng);
             // Commit to the random scalar via r := k · G.
             // This is the prover's first msg in the Sigma protocol.
             let prover_commitment = parameters.generator.mul(random_scalar).into_affine();
@@ -221,142 +226,6 @@ impl<ConstraintF: Field, C: ProjectiveCurve + ToConstraintField<ConstraintF>>
 /* MUSIG2 IMPLEMENTED BY IHYUN. SOURCE: https://github.com/conduition/musig2.git. 
     HARDCODED FOR BLS12-381. */
 
-impl<C: ProjectiveCurve + Hash> AggregateSignatureScheme for Schnorr<C>
-where
-    C::ScalarField: PrimeField,
-{
-    type Parameters = Parameters<C>;
-    type PublicKey = PublicKey<C>;
-    type SecretKey = SecretKey<C>;
-    type Signature = Signature<C>;
-
-    fn setup<R: Rng>(_rng: &mut R) -> Result<Self::Parameters, Error> {
-        let salt = None;
-        let generator = C::prime_subgroup_generator().into();
-
-        Ok(Parameters { generator, salt })
-    }
-
-    fn keygen<R: Rng>(
-        parameters: &Self::Parameters,
-        rng: &mut R,
-    ) -> Result<(Self::PublicKey, Self::SecretKey), Error> {
-        // Secret is a random scalar x
-        // the pubkey is y = xG
-        let secret_key: <<C as ProjectiveCurve>::Affine as AffineCurve>::ScalarField = C::ScalarField::rand(rng);
-        let public_key = parameters.generator.mul(secret_key).into();
-
-        Ok((
-            public_key,
-            SecretKey {
-                secret_key,
-                public_key,
-            },
-        ))
-    }
-    
-    fn sign<R: Rng>(
-        parameters: &Self::Parameters,
-        sk: &Self::SecretKey,
-        message: &[u8],
-        rng: &mut R,
-    ) -> Result<Self::Signature, Error> {
-        // (k, e);
-        let (random_scalar, verifier_challenge) = {
-            // Sample a random scalar `k` from the prime scalar field.
-            let random_scalar: C::ScalarField = C::ScalarField::rand(rng);
-            // Commit to the random scalar via r := k · G.
-            // This is the prover's first msg in the Sigma protocol.
-            let prover_commitment = parameters.generator.mul(random_scalar).into_affine();
-
-            // Hash everything to get verifier challenge.
-            // e := H(salt || pubkey || r || msg);
-            let mut hash_input = Vec::new();
-            if let Some(salt) = parameters.salt {
-                hash_input.extend_from_slice(&salt);
-            }
-            hash_input.extend_from_slice(&to_bytes![sk.public_key]?);
-            hash_input.extend_from_slice(&to_bytes![prover_commitment]?);
-            hash_input.extend_from_slice(message);
-
-            let hash_digest = Blake2s::digest(&hash_input);
-            assert!(hash_digest.len() >= 32);
-            let mut verifier_challenge = [0_u8; 32];
-            verifier_challenge.copy_from_slice(&hash_digest);
-
-            (random_scalar, verifier_challenge)
-        };
-
-        let verifier_challenge_fe = C::ScalarField::from_le_bytes_mod_order(&verifier_challenge);
-
-        // k - xe;
-        let prover_response = random_scalar - (verifier_challenge_fe * sk.secret_key);
-        let signature = Signature {
-            prover_response,
-            verifier_challenge,
-        };
-
-        Ok(signature)
-    }
-
-    fn verify(
-        parameters: &Self::Parameters,
-        pk: &Self::PublicKey,
-        message: &[u8],
-        signature: &Self::Signature,
-    ) -> Result<bool, Error> {
-        let Signature {
-            prover_response,
-            verifier_challenge,
-        } = signature;
-        let verifier_challenge_fe = C::ScalarField::from_le_bytes_mod_order(verifier_challenge);
-        // sG = kG - eY
-        // kG = sG + eY
-        // so we first solve for kG.
-        let mut claimed_prover_commitment = parameters.generator.mul(*prover_response);
-        let public_key_times_verifier_challenge = pk.mul(verifier_challenge_fe);
-        claimed_prover_commitment += &public_key_times_verifier_challenge;
-        let claimed_prover_commitment = claimed_prover_commitment.into_affine();
-
-        // e = H(salt, kG, msg)
-        let mut hash_input = Vec::new();
-        if let Some(salt) = parameters.salt {
-            hash_input.extend_from_slice(&salt);
-        }
-        hash_input.extend_from_slice(&to_bytes![pk]?);
-        hash_input.extend_from_slice(&to_bytes![claimed_prover_commitment]?);
-        hash_input.extend_from_slice(message);
-
-        // cast the hash output to get e
-        let obtained_verifier_challenge = &*Blake2s::digest(&hash_input);
-
-        // The signature is valid iff the computed verifier challenge is the same as the one
-        // provided in the signature
-        Ok(verifier_challenge == obtained_verifier_challenge)
-    }
-
-    // TODO: Implement
-    #[allow(clippy::todo)]
-    fn randomize_public_key(
-        _pp: &Self::Parameters,
-        _public_key: &Self::PublicKey,
-        _randomness: &[u8],
-    ) -> Result<Self::PublicKey, Error> {
-        todo!()
-    }
-
-    // TODO: Implement
-    #[allow(clippy::todo)]
-    fn randomize_signature(
-        _pp: &Self::Parameters,
-        _signature: &Self::Signature,
-        _randomness: &[u8],
-    ) -> Result<Self::Signature, Error> {
-        todo!()
-    }
-    
-}
-
 fn compute_key_aggregation_coefficient(
     pk_list_hash: &[u8; 32],
     pubkey: &Point,
@@ -366,21 +235,26 @@ fn compute_key_aggregation_coefficient(
     //     return MaybeScalar::one();
     // }
 
+    let mut bytes = [0u8; 32];
+    pubkey.serialize(&mut bytes[..]);
+    
     let hash: [u8; 32] = KEYAGG_COEFF_TAG_HASHER
         .clone()
         .chain_update(&pk_list_hash)
-        // .chain_update(&pubkey.serialize())
-        .chain_update(&pubkey)
+        .chain_update(&bytes)
+        // .chain_update(&pubkey)
         .finalize()
         .into();
 
-    Fr::from_le_bytes_mod_order(hash.to_bytes_le())
+    Fr::from_le_bytes_mod_order(&hash.to_vec())
 }
 
 fn hash_pubkeys<P: std::borrow::Borrow<Point>>(ordered_pubkeys: &[P]) -> [u8; 32] {
     let mut h = KEYAGG_LIST_TAG_HASHER.clone();
     for pubkey in ordered_pubkeys {
-        h.update(&pubkey.borrow().serialize());
+        let mut bytes = [0u8; 32];
+        pubkey.borrow().serialize(&mut bytes[..]);
+        h.update(&bytes);
     }
     h.finalize().into()
 }
@@ -388,7 +262,7 @@ fn hash_pubkeys<P: std::borrow::Borrow<Point>>(ordered_pubkeys: &[P]) -> [u8; 32
 #[derive(Debug, Clone)]
 pub struct KeyAggContext {
     /// The aggregated pubkey point `Q`.
-    pub(crate) pubkey: GroupAffine<P>,
+    pub(crate) pubkey: PublicKey<EdwardsProjective>,
     // pub(crate) pubkey: [u8;32],
 
     /// The component individual pubkeys in their original order.
@@ -402,13 +276,13 @@ pub struct KeyAggContext {
 
     /// Cached key aggregation coefficients of individual pubkeys, in the
     /// same order as `ordered_pubkeys`.
-    pub(crate) key_coefficients: Vec<C::ScalarField>,
+    pub(crate) key_coefficients: Vec<Fr>,
 
     /// A cache of effective individual pubkeys, i.e. `pubkey * self.key_coefficient(pubkey)`.
-    pub(crate) effective_pubkeys: Vec<GroupAffine<P>>,
+    pub(crate) effective_pubkeys: Vec<GroupAffine<EdwardsParameters>>,
 
     pub(crate) parity_acc: subtle::Choice, // false means g=1, true means g=n-1
-    pub(crate) tweak_acc: MaybeScalar,     // None means zero.
+    pub(crate) tweak_acc: Fr,     // None means zero.
 }
 
 impl KeyAggContext {
@@ -441,9 +315,8 @@ impl KeyAggContext {
 
         let pk_list_hash = hash_pubkeys(&ordered_pubkeys);
 
-        // TODO: maybe pubkey is not groupaffine?
         // NOTE: THIS DOESN'T CHECK FOR POINTS AT INFINITY. NOT READY FOR PRODUCTION.
-        let (effective_pubkeys, key_coefficients): (Vec<GroupAffine<P>>, Vec<C::ScalarField>) =
+        let (effective_pubkeys, key_coefficients): (Vec<GroupAffine<EdwardsParameters>>, Vec<Fr>) =
             ordered_pubkeys
                 .iter()
                 .map(|&pubkey| {
@@ -471,19 +344,19 @@ impl KeyAggContext {
             key_coefficients,
             effective_pubkeys,
             parity_acc: subtle::Choice::from(0),
-            tweak_acc: MaybeScalar::Zero,
+            tweak_acc: Fr::zero(),
         })
     }
 }
 
 pub struct SecNonce {
-    pub(crate) k1: SecretKey<C>,
-    pub(crate) k2: SecretKey<C>,
+    pub(crate) k1: SecretKey<EdwardsProjective>,
+    pub(crate) k2: SecretKey<EdwardsProjective>,
 }
 
 impl SecNonce {
     /// Construct a new `SecNonce` from the given individual nonce values.
-    pub fn new<T: Into<Scalar>>(k1: T, k2: T) -> SecNonce {
+    pub fn new<T: Into<SecretKey<EdwardsProjective>>>(k1: T, k2: T) -> SecNonce {
         SecNonce {
             k1: k1.into(),
             k2: k2.into(),
@@ -496,11 +369,18 @@ impl SecNonce {
     pub fn build<'snb>(nonce_seed: impl Into<NonceSeed>) -> SecNonceBuilder<'snb> {
         SecNonceBuilder::new(nonce_seed)
     }
+
+    pub fn public_nonce(&self) -> PubNonce {
+        PubNonce {
+            R1: self.k1.secret_key * G,        // G IS GENERATOR POINT
+            R2: self.k2.secret_key * G,
+        }
+    }
 }
 
 pub struct SecNonceBuilder<'snb> {
     nonce_seed_bytes: [u8; 32],
-    seckey: Option<SecretKey<C>>,
+    seckey: Option<SecretKey<EdwardsProjective>>,
     pubkey: Option<Point>,
     aggregated_pubkey: Option<Point>,
     message: Option<&'snb [u8]>,
@@ -569,14 +449,14 @@ impl<'snb> SecNonceBuilder<'snb> {
     /// Overwrites any public key previously added by
     /// [`SecNonceBuilder::with_pubkey`], as we compute the public key
     /// of the given secret key and add it to the builder.
-    pub fn with_seckey(self, seckey: impl Into<Scalar>) -> SecNonceBuilder<'snb> {
-        let seckey: Scalar = seckey.into();
-        SecNonceBuilder {
-            seckey: Some(seckey),
-            pubkey: Some(seckey * G),
-            ..self
-        }
-    }
+    // pub fn with_seckey(self, seckey: impl Into<Fr>) -> SecNonceBuilder<'snb> {
+    //     let seckey: Fr = seckey.into();
+    //     SecNonceBuilder {
+    //         seckey: Some(seckey),
+    //         pubkey: Some(seckey * G),
+    //         ..self
+    //     }
+    // }
 
     /// Salt the resulting nonce with the message which we expect to be signing with
     /// the nonce.
@@ -618,29 +498,29 @@ impl<'snb> SecNonceBuilder<'snb> {
     ///     .with_extra_input(&String::from("What's up buttercup?"))
     ///     .build();
     /// ```
-    pub fn with_extra_input<E: AsRef<[u8]>>(
-        mut self,
-        extra_input: &'snb E,
-    ) -> SecNonceBuilder<'snb> {
-        self.extra_inputs.push(extra_input);
-        extra_input_length_check(&self.extra_inputs);
-        self
-    }
+    // pub fn with_extra_input<E: AsRef<[u8]>>(
+    //     mut self,
+    //     extra_input: &'snb E,
+    // ) -> SecNonceBuilder<'snb> {
+    //     self.extra_inputs.push(extra_input);
+    //     extra_input_length_check(&self.extra_inputs);
+    //     self
+    // }
 
     /// Sprinkles in a set of [`SecNonceSpices`] to this nonce builder. Extra inputs in
     /// `spices` are appended to the builder (see [`SecNonceBuilder::with_extra_input`]).
     /// All other parameters will be merged with those in `spices`, preferring parameters
     /// in `spices` if they are present.
-    pub fn with_spices(mut self, spices: SecNonceSpices<'snb>) -> SecNonceBuilder<'snb> {
-        self.seckey = spices.seckey.or(self.seckey);
-        self.message = spices.message.map(|msg| msg.as_ref()).or(self.message);
+    // pub fn with_spices(mut self, spices: SecNonceSpices<'snb>) -> SecNonceBuilder<'snb> {
+    //     self.seckey = spices.seckey.or(self.seckey);
+    //     self.message = spices.message.map(|msg| msg.as_ref()).or(self.message);
 
-        let mut new_extra_inputs = spices.extra_inputs;
-        self.extra_inputs.append(&mut new_extra_inputs);
-        extra_input_length_check(&self.extra_inputs);
+    //     let mut new_extra_inputs = spices.extra_inputs;
+    //     self.extra_inputs.append(&mut new_extra_inputs);
+    //     extra_input_length_check(&self.extra_inputs);
 
-        self
-    }
+    //     self
+    // }
 
     /// Build the secret nonce by hashing all of the builder's inputs into two
     /// byte arrays, and reducing those byte arrays modulo the curve order into
@@ -724,15 +604,22 @@ impl<'snb> SecNonceBuilder<'snb> {
         let hash1 = <[u8; 32]>::from(hasher.clone().chain_update(&[0]).finalize());
         let hash2 = <[u8; 32]>::from(hasher.clone().chain_update(&[1]).finalize());
 
-        let k1 = match MaybeScalar::reduce_from(&hash1) {
-            MaybeScalar::Zero => Scalar::one(),
-            MaybeScalar::Valid(k) => k,
+        let k1: Fr = Fr::from_le_bytes_mod_order(&hash1);
+        let k1: Fr = if k1.is_zero() {
+            Fr::one()
+        } else {
+            k1
         };
-        let k2 = match MaybeScalar::reduce_from(&hash2) {
-            MaybeScalar::Zero => Scalar::one(),
-            MaybeScalar::Valid(k) => k,
+        let k2: Fr = Fr::from_le_bytes_mod_order(&hash2);
+        let k2: Fr = if k2.is_zero() {
+            Fr::one()
+        } else {
+            k2
         };
-        SecNonce { k1, k2 }
+
+        let seckey1 = SecretKey {secret_key: k1, public_key: PublicKey::default()};
+        let seckey2 = SecretKey {secret_key: k2, public_key: PublicKey::default()};
+        SecNonce { k1: seckey1, k2: seckey2 }
     }
 }
 
@@ -741,12 +628,50 @@ struct Slots<T: Clone + Eq> {
     open_slots: Vec<usize>,
 }
 
+impl<T: Clone + Eq> Slots<T> {
+    /// Create a new set of slots.
+    fn new(expected_size: usize) -> Slots<T> {
+        let mut slots = Vec::new();
+        slots.resize(expected_size, None);
+        let open_slots = Vec::from_iter(0..expected_size);
+        Slots { slots, open_slots }
+    }
+
+    /// Add an item to a specific slot, returning an error if the
+    /// slot is already taken by a different item. Idempotent.
+    fn place(&mut self, value: T, index: usize) -> Result<(), RoundContributionError> {
+        if index >= self.slots.len() {
+            return Err(RoundContributionError::out_of_range(
+                index,
+                self.slots.len(),
+            ));
+        }
+
+        // Support idempotence. Callers can place the same value into the same
+        // slot index, which should be a no-op.
+        if let Some(ref existing) = self.slots[index] {
+            if &value == existing {
+                return Ok(());
+            } else {
+                return Err(RoundContributionError::inconsistent_contribution(index));
+            }
+        }
+
+        self.slots[index] = Some(value);
+        self.open_slots
+            .remove(self.open_slots.binary_search(&index).unwrap());
+        Ok(())
+    }
+}
+
 // #[derive(Debug, Eq, PartialEq, Clone, Hash, Ord, PartialOrd)]
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
 pub struct PubNonce {
     pub R1: Point,
     pub R2: Point,
 }
+
+
 
 /// A state machine which manages the first round of a MuSig2 signing session.
 ///
@@ -793,13 +718,13 @@ impl FirstRound {
         let secnonce = SecNonce::build(nonce_seed)
             .with_pubkey(signer_pubkey)
             .with_aggregated_pubkey(aggregated_pubkey)
-            .with_extra_input(&(signer_index as u32).to_be_bytes())
-            // .with_spices(spices)
+            // .with_extra_input(&(signer_index as u32).to_be_bytes())  // SEEMS EXTRA
+            // .with_spices(spices)     // SEEMS EXTRA
             .build();
 
         let pubnonce = secnonce.public_nonce();
 
-        let mut pubnonce_slots = Slots::new(key_agg_ctx.pubkeys().len());
+        let mut pubnonce_slots = Slots::new(key_agg_ctx.ordered_pubkeys.len());
         pubnonce_slots.place(pubnonce, signer_index).unwrap(); // never fails
 
         Ok(FirstRound {
