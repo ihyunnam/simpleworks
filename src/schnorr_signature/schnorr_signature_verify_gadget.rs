63ae405019fb566dddb417de4f06cc2b07173172
Ihyun Nam <72367427+ihyunnam@users.noreply.github.com>
2024-08-23 20:58:37 -0700

    Update schnorr_signature_verify_gadget.rs

--- src/schnorr_signature/schnorr_signature_verify_gadget.rs
+++ src/schnorr_signature/schnorr_signature_verify_gadget.rs
@@ -109,8 +109,8 @@ where
 
 //         println!("verification_point_var {:?}", verification_point_var.value());
 
-        // verification_point_var.is_eq(verifier_challenge.as_slice())
-        Ok(ark_r1cs_std::prelude::Boolean::Constant(true))
+        verification_point_var.is_eq(verifier_challenge.as_slice())
+        // Ok(ark_r1cs_std::prelude::Boolean::Constant(true))
 
     }
 }
