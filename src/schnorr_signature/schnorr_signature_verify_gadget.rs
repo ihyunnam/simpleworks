801ab5d3dba3f2a3016f3f324c711c856c4987fe
Ihyun Nam <ihyunnam@DNa809943.SUNet>
2024-08-25 22:36:03 -0700

    for timing purposes

--- src/schnorr_signature/schnorr_signature_verify_gadget.rs
+++ src/schnorr_signature/schnorr_signature_verify_gadget.rs
@@ -83,17 +83,17 @@ where
         let verifier_challenge = signature.verifier_challenge.clone();
 
         // let poseidon_params = poseidon_params.params;
-        let poseidon_params = PoseidonRoundParamsVar::<ConstraintF<C>, MyPoseidonParams>::new_variable(
-            cs.clone(),
-            || Ok(poseidon_params.params),
-            AllocationMode::Witness,
-        )?;
+        // let poseidon_params = PoseidonRoundParamsVar::<ConstraintF<C>, MyPoseidonParams>::new_variable(
+        //     cs.clone(),
+        //     || Ok(poseidon_params.params),
+        //     AllocationMode::Witness,
+        // )?;
 
-        let mut hash1_var = vec![];
-        for coord in verifier_challenge.value().unwrap_or(vec![0u8;32]) {
-            // println!("coord vc {:?}", coord);
-            hash1_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Witness).unwrap());
-        }
+        // let mut hash1_var = vec![];
+        // for coord in verifier_challenge.value().unwrap_or(vec![0u8;32]) {
+        //     // println!("coord vc {:?}", coord);
+        //     hash1_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Witness).unwrap());
+        // }
 
         let pubkey_affine = public_key.pub_key.value().unwrap_or(C::default()).into_affine();
         let mut agg_pubkey_serialized = [0u8; 32];
@@ -103,15 +103,15 @@ where
         for coord in agg_pubkey_serialized {
             hash2_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Witness).unwrap());
         }
-        let mut hash3_var = vec![];
-        for coord in message.value().unwrap_or(vec![0u8;96]) {
-            // println!("coord msg {:?}", coord);
-            hash3_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Witness).unwrap());
-        }
+        // let mut hash3_var = vec![];
+        // for coord in message.value().unwrap_or(vec![0u8;96]) {
+        //     // println!("coord msg {:?}", coord);
+        //     hash3_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Witness).unwrap());
+        // }
 
-        let hash1 = CRHGadget::evaluate(&poseidon_params, &hash1_var).unwrap();
-        let hash2 = CRHGadget::evaluate(&poseidon_params, &hash2_var).unwrap();
-        let hash3 = CRHGadget::evaluate(&poseidon_params, &hash3_var).unwrap();
+        let hash1 = CRHGadget::<ConstraintF<C>, MyPoseidonParams>::evaluate(&poseidon_params, &verifier_challenge).unwrap();
+        let hash2 = CRHGadget::<ConstraintF<C>, MyPoseidonParams>::evaluate(&poseidon_params, &hash2_var).unwrap();
+        let hash3 = CRHGadget::<ConstraintF<C>, MyPoseidonParams>::evaluate(&poseidon_params, &message).unwrap();
 
         let mut vector1 = vec![];
         let mut vector2 = vec![];
@@ -129,17 +129,21 @@ where
         hash1.serialize(&mut vector1).unwrap();
         hash2.serialize(&mut vector2).unwrap();
         hash3.serialize(&mut vector3).unwrap();
+
+        // UInt8::<ConstraintF<C>>::new_witness_vec(cs.clone(), vector1.as_slice());
+        // UInt8::<ConstraintF<C>>::new_witness_vec(cs.clone(), vector2.as_slice());
+        // UInt8::<ConstraintF<C>>::new_witness_vec(cs.clone(), vector3.as_slice());
+
         let mut final_vector = Vec::with_capacity(vector1.len() + vector2.len() + vector3.len());
         final_vector.extend(vector1.clone());
         final_vector.extend(vector2.clone());
         final_vector.extend(vector3.clone());
 
+        // let final_vector_1 = UInt8::<ConstraintF<C>>::new_witness_vec(cs.clone(), final_vector.as_slice());
+
+
         let e = C::ScalarField::from_be_bytes_mod_order(final_vector.as_slice());          // to_bytes in LE
         
-        // let vector1 = UInt8::<ConstraintF<C>>::new_witness_vec(cs.clone(), vector1.as_slice());
-        // let vector2 = UInt8::<ConstraintF<C>>::new_witness_vec(cs.clone(), vector2.as_slice());
-        // let vector3 = UInt8::<ConstraintF<C>>::new_witness_vec(cs.clone(), vector3.as_slice());
-
         // let verification_point = parameters.generator.mul(prover_response).sub(pubkey_affine.mul(e));
 
         let verification_point = parameters.generator.value().unwrap_or(C::default()).into_affine().mul(prover_response_fe).sub(public_key.pub_key.value().unwrap_or(C::default()).into_affine().mul(e)).into_affine();
@@ -148,68 +152,69 @@ where
         let end = start.elapsed();
         println!("verify 4 {:?}", end);
         
-        let verification_point_wtns = UInt8::<ConstraintF<C>>::new_witness_vec (
-            cs.clone(),
-            &verification_point_bytes
-        ).unwrap();
+        let mut verification_point_wtns: Vec<UInt8<ConstraintF<C>>> = vec![];
+        for coord in verification_point_bytes {
+            verification_point_wtns.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Witness).unwrap());
+        }
         
         Ok(verification_point_wtns.is_eq(&verifier_challenge)?)
-
-        // let prover_response = signature.prover_response.clone();
-        // let verifier_challenge = signature.verifier_challenge.clone();
-        
-        // let pubkey_affine = public_key.pub_key.value().unwrap_or(C::default()).into_affine();
-
-        // println!("pubkey affine {:?}", pubkey_affine);
-        // let mut agg_pubkey_serialized = [0u8; 32];
-        // pubkey_affine.serialize(&mut agg_pubkey_serialized[..]);
-
-        // let mut reader = Cursor::new(prover_response.value().unwrap_or([0u8;32].to_vec()));
-
-        // // Deserialize the bytes back into an affine point
-        // let prover_response_fe = C::ScalarField::deserialize(&mut reader).unwrap();
-     
-        // let mut hash_var: Vec<UInt8<ConstraintF<C>>> = vec![];
-        // for coord in verifier_challenge.value().unwrap_or(vec![0u8;32]) {
-        //     // println!("coord vc {:?}", coord);
-        //     hash_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Constant).unwrap());
-        // }
-        // for coord in agg_pubkey_serialized {
-        //     // println!("coord ap {:?}", coord);
-        //     hash_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Constant).unwrap());
-        // }
-        // for coord in message.value().unwrap_or(vec![0u8;96]) {
-        //     // println!("coord msg {:?}", coord);
-        //     hash_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Constant).unwrap());
-        // }
-
-        // // println!("hash_var {:?}", hash_var.value());
-
-        // // let b2s_params: Blake2sParametersVar = <Blake2sParametersVar as AllocVar<_, ConstraintF<C>>>::new_constant(
-        // //     ConstraintSystemRef::None,
-        // //     (),
-        // // )?;
-
-        // let hash = CRHGadget::evaluate(&poseidon_params, &hash_var)?;
-        // // println!("HASH {:?}", hash.value());
-
-        // let e = C::ScalarField::from_be_bytes_mod_order(&hash.value().unwrap_or([0u8;32]));
-
-        // let verification_point = parameters.generator.value().unwrap_or(C::default()).into_affine().mul(prover_response_fe).sub(public_key.pub_key.value().unwrap_or(C::default()).into_affine().mul(e)).into_affine();
-
-        // let mut verification_point_bytes: Vec<u8> = vec![];
-        // verification_point.serialize(&mut verification_point_bytes);
-
-        // let mut verification_point_var: Vec<UInt8<ConstraintF<C>>> = vec![];
-        // for coord in verification_point_bytes {
-        //     verification_point_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Constant).unwrap());
-        // }
-        
-        // // verification_point_var.enforce_equal(&verifier_challenge);
-        // // println!("RESULT {:?}", result.value());
-        
-        // // Dummy return value
-        // Ok(verification_point_var.is_eq(&verifier_challenge)?)
-
     }
 }
+
+
+
+// let prover_response = signature.prover_response.clone();
+// let verifier_challenge = signature.verifier_challenge.clone();
+
+// let pubkey_affine = public_key.pub_key.value().unwrap_or(C::default()).into_affine();
+
+// println!("pubkey affine {:?}", pubkey_affine);
+// let mut agg_pubkey_serialized = [0u8; 32];
+// pubkey_affine.serialize(&mut agg_pubkey_serialized[..]);
+
+// let mut reader = Cursor::new(prover_response.value().unwrap_or([0u8;32].to_vec()));
+
+// // Deserialize the bytes back into an affine point
+// let prover_response_fe = C::ScalarField::deserialize(&mut reader).unwrap();
+
+// let mut hash_var: Vec<UInt8<ConstraintF<C>>> = vec![];
+// for coord in verifier_challenge.value().unwrap_or(vec![0u8;32]) {
+//     // println!("coord vc {:?}", coord);
+//     hash_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Constant).unwrap());
+// }
+// for coord in agg_pubkey_serialized {
+//     // println!("coord ap {:?}", coord);
+//     hash_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Constant).unwrap());
+// }
+// for coord in message.value().unwrap_or(vec![0u8;96]) {
+//     // println!("coord msg {:?}", coord);
+//     hash_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Constant).unwrap());
+// }
+
+// // println!("hash_var {:?}", hash_var.value());
+
+// // let b2s_params: Blake2sParametersVar = <Blake2sParametersVar as AllocVar<_, ConstraintF<C>>>::new_constant(
+// //     ConstraintSystemRef::None,
+// //     (),
+// // )?;
+
+// let hash = CRHGadget::evaluate(&poseidon_params, &hash_var)?;
+// // println!("HASH {:?}", hash.value());
+
+// let e = C::ScalarField::from_be_bytes_mod_order(&hash.value().unwrap_or([0u8;32]));
+
+// let verification_point = parameters.generator.value().unwrap_or(C::default()).into_affine().mul(prover_response_fe).sub(public_key.pub_key.value().unwrap_or(C::default()).into_affine().mul(e)).into_affine();
+
+// let mut verification_point_bytes: Vec<u8> = vec![];
+// verification_point.serialize(&mut verification_point_bytes);
+
+// let mut verification_point_var: Vec<UInt8<ConstraintF<C>>> = vec![];
+// for coord in verification_point_bytes {
+//     verification_point_var.push(UInt8::new_variable(cs.clone(), || Ok(coord), AllocationMode::Constant).unwrap());
+// }
+
+// // verification_point_var.enforce_equal(&verifier_challenge);
+// // println!("RESULT {:?}", result.value());
+
+// // Dummy return value
+// Ok(verification_point_var.is_eq(&verifier_challenge)?)
